cf3886ba727530fa52c51d522a1705836982c468
Vinicius <vinicius.cesar@uel.br>
2021-05-03 21:27:22 -0300

    Removendo código inútil.

--- src/main/java/com/uel/ZZCommerce/controller/LoginServlet.java
+++ src/main/java/com/uel/ZZCommerce/controller/LoginServlet.java
@@ -18,8 +18,6 @@ import javax.servlet.http.*;
         }
 )
 public class LoginServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
- 
     public LoginServlet() {
         super();
     }
